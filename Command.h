3b9c883c7de6ab2a2c1d9406ef9e55f6e7cb1f0b
jamelouis <jamelouis2015@gmail.com>
2018-07-03 16:51:31 +0800

    basis ReadDirectoryChangesW usage.

---
+++ Command.h
@@ -0,0 +1,48 @@
+#pragma once
+/// Command - Game Programming Patterns
+// http://gameprogrammingpatterns.com/command.html
+
+#include <stdio.h>
+
+class Command
+{
+public:
+	virtual ~Command() {};
+	virtual void execute(const char* filename, const char* filename2 = NULL) = 0;
+};
+
+class AddCommand : public Command
+{
+public:
+	virtual void execute(const char* filename, const char* filename2)
+	{
+		printf("New file/Dir: %s\n", filename);
+	}
+};
+
+class ModifyCommand : public Command
+{
+public:
+	virtual void execute(const char* filename, const char* filename2)
+	{
+		printf("the file was modified.: %s\n", filename);
+	}
+};
+
+class RemoveCommand : public Command
+{
+public:
+	virtual void execute(const char* filename, const char* filename2)
+	{
+		printf("removed: %s\n", filename);
+	}
+};
+
+class RenameCommand : public Command
+{
+public:
+	virtual void execute(const char* filename, const char* filename2)
+	{
+		printf("rename %s to %s.\n", filename, filename2);
+	}
+};
